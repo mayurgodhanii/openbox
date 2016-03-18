9f7963ed1a4ab65ec6df1335cb4491df4f0abeae
Mayur Godhani <mayurgodhani2000@gmail.com>
2016-03-18 12:42:36 +0530

    Update code version 2

--- openbox/openbox.js
+++ openbox/openbox.js
@@ -7,7 +7,37 @@ $(document).ready(function () {
             manageright();
         }
     });
+
+    $(document).on('click', '.google-diolog-box', function (event) {
+        event.preventDefault();
+        var link = $(this).attr("href");
+        var this_element = $(this);
+        this_element.text("Submiting.....");
+        this_element.attr("disabled", true);
+        $.ajax({
+            url: link,
+        }).done(function (data) {
+            $(".diolog-box-area").append(data);
+            manageright();
+            this_element.text("Submit");
+            this_element.attr("disabled", false);
+        });
+    });
 });
+
+(function ($) {
+    var $window = $(window);
+
+    function resize() {
+        if ($window.width() < 514) {
+            return $(".google-diolog-box").removeClass('google-diolog-box').addClass('ajaxviewmodel');
+        }
+        $(".ajaxviewmodel").removeClass('ajaxviewmodel').addClass('google-diolog-box')
+    }
+    $window
+            .resize(resize)
+            .trigger('resize');
+})(jQuery);
 
 function manageright() {
     var left = 1;
@@ -15,18 +45,18 @@ function manageright() {
     var total_block = 0;
     $(".diolog-box-area .r-diolog-box").each(function (index) {
         var children_div = $(this).children();
-        if (total_block >= 2) {
+        if (total_block >= 1) {
             children_div.addClass("collapsed-box");
             $(this).find(".minimise-box i").removeClass("fa fa-minus").addClass("fa fa-plus");
         }
-        if (total_block >= 3) {
+        if (total_block >= 2) {
             $(this).attr('style', 'bottom:' + bottom + 'px;right:' + left + "%");
             bottom = bottom + 50;
         } else {
             $(this).attr('style', 'right:' + left + "%");
         }
-        if (total_block < 2) {
-            left = left + 33;
+        if (total_block < 1) {
+            left = left + 49
         }
 
         total_block = total_block + 1;
